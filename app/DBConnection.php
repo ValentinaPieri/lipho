dcad8299cddffd145d79b5c7e9abb18372a6cba9
expl0r3rgu1 <salvaddimando@gmail.com>
2022-12-19 16:47:27 +0100

    DBConnection not a class anymore

--- app/DBConnection.php
+++ app/DBConnection.php
@@ -4,21 +4,9 @@ const host = 'detu.ddns.net';
 const user = 'lipho';
 const passw = 'Lipho@';
 const db = 'lipho';
+const port = 3306;
 
-class DBConnection
-{
-    private $conn;
-
-    public function __construct()
-    {
-        $this->conn = new mysqli(host, user, passw, db);
-        if ($this->conn->connect_error) {
-            echo "Connection failed: " . $this->conn->connect_error;
-        }
-    }
-
-    public function getConnection()
-    {
-        return $this->conn;
-    }
+$conn = new mysqli(host, user, passw, db, port);
+if ($this->conn->connect_error) {
+    echo "Connection failed: " . $this->conn->connect_error;
 }
