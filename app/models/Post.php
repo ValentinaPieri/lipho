850cd164802e799f5e2d4efe0a2b55ef348ecb1c
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-12-21 19:14:06 +0100

    Post class refactoring

--- app/models/Post.php
+++ app/models/Post.php
@@ -1,23 +1,26 @@
 <?php
 
 namespace app\models;
-use const app\QUERY;
 
-require_once('../DBConnection.php');
+require_once 'app/query.php';
 
-class Post 
+class Post
 {
     private int $post_id;
     private string $username;
     private string $caption;
+    private string $timestamp;
     private $images;
     private $likes;
     private $comments;
     private float $avg_exposure_rating;
     private float $avg_colors_rating;
     private float $avg_composition_rating;
+    private $conn;
 
-    public function __construct($username, $caption, $images) {
+    public function __construct($username, $caption, $conn, $images = array(), $post_id = 0, $timestamp = "")
+    {
+        $this->post_id = $post_id;
         $this->caption = $caption;
         $this->username = $username;
         $this->avg_exposure_rating = 0;
@@ -26,70 +29,78 @@ class Post
         $this->likes = array();
         $this->comments = array();
         $this->images = $images;
-    }   
-
-    public function create_new() {
-        global $conn;
-        $stmt = $conn->prepare(QUERY['add_post']);
-        $stmt->bind_param("ss", $this->caption, $this->username);
-        $stmt->execute();
-        $this->post_id = $conn->insert_id;
+        $this->timestamp = $timestamp;
+        $this->conn = $conn;
+        if ($post_id == 0) {
+            $this->createNew();
+        }
     }
 
-    public function add_images($images) {
-        global $conn;
-        if(isset($images) && !empty($images)) {
-            $stmt = $conn->prepare(QUERY['add_post_image']);
-            for ($i = 0; $i < count($images); $i++) {
-                $image = base64_encode($images[$i]);
+    public function createNew()
+    {
+        $stmt = $this->conn->prepare(QUERIES['add_post']);
+        $stmt->bind_param("ss", $this->caption, $this->username);
+        $stmt->execute();
+        $this->post_id = $this->conn->insert_id;
+        $this->addImages();
+    }
+
+    private function addImages()
+    {
+        if (isset($this->images) && !empty($this->images)) {
+            $stmt = $this->conn->prepare(QUERIES['add_post_image']);
+            for ($i = 0; $i < count($this->images); $i++) {
+                $image = $this->images[$i];
                 $stmt->bind_param("iis", $this->post_id, $i, $image);
                 $stmt->execute();
             }
         }
     }
 
-    public function delete() {
-        global $conn;
-        $stmt = $conn->prepare(QUERY['delete_post']);
+    public function delete()
+    {
+        $stmt = $this->conn->prepare(QUERIES['delete_post']);
         $stmt->bind_param("i", $this->post_id);
         $stmt->execute();
     }
 
-    public function like($username) {
-        global $conn;
-        $stmt = $conn->prepare(QUERY['like_post']);
+    public function like($username)
+    {
+        array_push($this->likes, $username);
+        $stmt = $this->conn->prepare(QUERIES['like_post']);
         $stmt->bind_param("is", $this->post_id, $username);
         $stmt->execute();
-        $this->likes[] = $username;
     }
 
-    public function unlike($username) {
-        global $conn;
-        $stmt = $conn->prepare(QUERY['unlike_post']);
+    public function unlike($username)
+    {
+        $stmt = $this->conn->prepare(QUERIES['unlike_post']);
         $stmt->bind_param("is", $this->post_id, $username);
         $stmt->execute();
         $this->likes = array_diff($this->likes, array($username));
     }
 
-    public function comment($text, $username) {
-        $comment = new Comment($text, $this->post_id, $username);
-        $this->comments[] = $comment;
+    public function comment($text, $username)
+    {
+        $comment = new Comment($text, $this->post_id, $username, $this->conn);
+        array_push($this->comments, $comment);
     }
 
-    public function uncomment($comment) {
+    public function uncomment($comment)
+    {
         $this->comments = array_diff($this->comments, $comment);
         $comment->delete();
     }
 
-    public function rate($username, $exposure, $colors, $composition) {
-        global $conn;
-        $stmt = $conn->prepare(QUERY['rate_post']);
+    public function rate($username, $exposure, $colors, $composition)
+    {
+        $stmt = $this->conn->prepare(QUERIES['rate_post']);
         $stmt->bind_param("isiii", $this->post_id, $username, $exposure, $colors, $composition);
         $stmt->execute();
-        $stmt = $conn->prepare(QUERY['update_average_post_rating']);
-        $stmt->bind_param("i", $this->post_id);
+        $stmt = $this->conn->prepare(QUERIES['update_average_post_rating']);
+        $stmt->bind_param("iiii", $this->post_id, $this->post_id, $this->post_id, $this->post_id);
         $stmt->execute();
-        $stmt = $conn->prepare(QUERY['get_average_post_rating']);
+        $stmt = $this->conn->prepare(QUERIES['get_average_post_rating']);
         $stmt->bind_param("i", $this->post_id);
         $stmt->execute();
         $result = $stmt->get_result()->fetch_assoc();
@@ -98,4 +109,3 @@ class Post
         $this->avg_composition_rating = $result['composition'];
     }
 }
-?>
