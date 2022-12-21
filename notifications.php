930ba1c6ea64affc7e3dd7e880fc552524782627
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-12-21 18:20:03 +0100

    Testing the notification send method

--- notifications.php
+++ notifications.php
@@ -1,13 +1,17 @@
 <?php
 require_once 'app/DBConnection.php';
+require_once 'app/models/Notification.php';
 
 use app\DBConnection;
+use app\models\Notification;
 
 $templateParams["title"] = "Notifications";
 
 $dbconnection = new DBConnection();
-$notifications = $dbconnection->getNotifications();
 
-$templateParams["page"] = $notifications[0]->getUsername();
+$notification = new Notification("test",false, "test", $dbconnection->getConnection());
+
+$notifications = $dbconnection->getNotifications();
+$templateParams["page"] = $notifications[0]->getText();
 
 require_once 'templates/base.php';
