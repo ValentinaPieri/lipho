59f5e3c3fcf970024cc931a0dd8f22ae6a36105a
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2022-12-21 14:11:33 +0100

    Created method getNotifications in DBConnection

--- app/DBConnection.php
+++ app/DBConnection.php
@@ -1,5 +1,9 @@
 <?php
 
+use const app\QUERY;
+
+require_once 'models/Notification.php';
+
 const host = 'detu.ddns.net';
 const user = 'lipho';
 const passw = 'Lipho@';
@@ -21,5 +25,18 @@ class DBConnection
     public function getConnection()
     {
         return $this->conn;
+    }
+
+    public function getNotifications()
+    {
+        $result = $this->conn->query(QUERY['get_user_notifications']);
+        $notifications = array();
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $notification = new Notification($row['notification_id'], $row['text'], $row['seen'], $row['timestamp'], $row['username'], $this->conn);
+                array_push($notifications, $notification);
+            }
+        }
+        return $notifications;
     }
 }
