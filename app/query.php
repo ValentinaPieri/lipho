d968e1447d2b9665a2b3beda72c08a4c5ac7b795
expl0r3rgu1 <salvaddimando@gmail.com>
2022-12-23 11:33:19 +0100

    Created get_user_profile_image query

--- app/query.php
+++ app/query.php
@@ -27,4 +27,5 @@ const QUERIES = [
     "delete_notification" => "DELETE FROM notification WHERE notification_id = ?",
     "clear_user_notifications" => "DELETE FROM notification WHERE username = ?",
     "send_notification" => "INSERT INTO notification (`text`, seen, username) VALUES (?, ?, ?)",
+    "get_user_profile_image" => "SELECT profile_image FROM user WHERE username = ?",
 ];
