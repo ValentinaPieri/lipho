c0b1a4ba71a1dc87424ba410acb270a4e409842b
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-12-25 22:39:19 +0100

    Wrote template signup.php

---
+++ templates/signup.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lipho | <?php echo $templateParams["title"]; ?></title>
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.2.1/css/all.css">
+</head>
+
+<main>
+    <div class="page">
+        <div class="header">
+            <h1 id="SignUp">Sign up</h1>
+        </div>
+        <div class="container">
+            <form action="" method="post">
+                <i class="fa-solid fa-asterisk"></i><input type="text" placeholder="username"><br>
+                <i class="fa-solid fa-asterisk"></i><input type="password" id="password1" placeholder="password"><br>
+                <i class="fa-solid fa-asterisk"></i><input type="password" id="password2"
+                    placeholder="confirm password"><br>
+                <i class="fa-solid fa-asterisk"></i><input type="text" placeholder="name"><br>
+                <i class="fa-solid fa-asterisk"></i><input type="text" placeholder="surname"><br>
+                <input type="text" placeholder="email"><br>
+                <input type="text" placeholder="phone"><br>
+                <label for="birthday">birthday:</label>&nbsp;
+                <input type="date" id="birthday" name="birthday"><i class="fa-regular fa-calendar"></i><br><br>
+                <button type="submit" name="sign-up">Sign up</button>
+            </form>
+        </div>
+    </div>
+    <div class="option">
+        <p>Already have an account? <a href="">Login now</a></p>
+    </div>
+</main>
+
+</html>
