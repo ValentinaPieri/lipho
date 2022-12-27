5feac70986ca9c4115876df05f8e8922350e7ba9
expl0r3rgu1 <salvaddimando@gmail.com>
2022-12-27 13:12:40 +0100

    Created post requests handler script

---
+++ post_requests_handler.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'app/DBConnection.php';
+
+use app\DBConnection;
+
+if (isset($_POST['notificationId'])) {
+    $index = intval($_POST['notificationId']);
+    $dbconnection = new DBConnection();
+    $dbconnection->deleteNotification($index);
+}
