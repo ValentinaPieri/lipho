2363139bd432a2cda0731c99ae51401de1f6dc60
expl0r3rgu1 <salvaddimando@gmail.com>
2022-12-28 12:55:14 +0100

    Created deleteAllNotifications method in notifications js script

--- js/notifications.js
+++ js/notifications.js
@@ -9,3 +9,13 @@ function deleteNotification(notificationId) {
             location.reload();
         });
 }
+
+function deleteAllNotifications() {
+    $.post("/lipho/post_requests_handler.php", { deleteAllNotifications: true })
+        .done(function (result) {
+            if (result != "") {
+                alert(result);
+            }
+            location.reload();
+        });
+}
