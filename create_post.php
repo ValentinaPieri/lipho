1b81d54cd6f3444d159e0d752fc8e77dfea40a94
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-12-29 14:36:53 +0100

    Moved the creation of the post in the db from create_post.php to post_requests_handler.php

--- create_post.php
+++ create_post.php
@@ -8,14 +8,9 @@ use app\models\Post;
 
 $templateParams["title"] = "Create Post";
 $templateParams["scripts"] = "<script src='js/posts.js'></script>";
-
-$dbconnection = new DBConnection();
-$conn = $dbconnection->getConnection();
-
 $templateParams["page"] = "";
-
 $templateParams["page"] .= "
-<form method='post' enctype='multipart/form-data'>
+<form action='post_requests_handler.php' method='post' enctype='multipart/form-data'>
 
     <div id='images-form'>
     <label for='caption'><h2>Pictures</h2></label>
@@ -33,24 +28,5 @@ $templateParams["page"] .= "
     <button type='submit' class='btn' name='post-button'>Post</button>
 </form>
 ";
-
-if (isset($_POST["post-button"])) {
-    $images = array();
-    $username = "test";
-
-    for ($i = 0; $i < 5; $i++) {
-        if (isset($_FILES['image-input' . $i])) {
-            $fileName = basename($_FILES['image-input' . $i]['name']);
-            $fileType = pathinfo($fileName, PATHINFO_EXTENSION);
-            $allowedTypes = array('jpg', 'png', 'jpeg');
-            if (in_array($fileType, $allowedTypes)) {
-                $image = $_FILES['image-input' . $i]['tmp_name'];
-                $imgContent = addslashes(file_get_contents($image));
-                array_push($images, $imgContent);
-            }
-        }
-    }
-    $post = new Post(/*$_SESSION["username"]*/$username, $_POST["caption"], $conn, $images);
-}
 
 require_once 'templates/base.php';
--- post_requests_handler.php
+++ post_requests_handler.php
@@ -3,6 +3,7 @@
 require_once 'app/DBConnection.php';
 
 use app\DBConnection;
+use app\models\Post;
 
 if (isset($_POST['notificationId'])) {
     $index = intval($_POST['notificationId']);
@@ -13,4 +14,25 @@ if (isset($_POST['notificationId'])) {
 if (isset($_POST['deleteAllNotifications'])) {
     $dbconnection = new DBConnection();
     $dbconnection->deleteAllNotifications();
+}
+
+if (isset($_POST["post-button"])) {
+    $dbconnection = new DBConnection();
+    $conn = $dbconnection->getConnection();
+    $images = array();
+    $username = "test"; //TODO: change this to the current user
+    for ($i = 0; $i < 5; $i++) {
+        if (isset($_FILES['image-input' . $i])) {
+            $fileName = basename($_FILES['image-input' . $i]['name']);
+            $fileType = pathinfo($fileName, PATHINFO_EXTENSION);
+            $allowedTypes = array('jpg', 'png', 'jpeg');
+            if (in_array($fileType, $allowedTypes)) {
+                $image = $_FILES['image-input' . $i]['tmp_name'];
+                $imgContent = addslashes(file_get_contents($image));
+                array_push($images, $imgContent);
+            }
+        }
+    }
+    $post = new Post(/*$_SESSION["username"]*/$username, $_POST["caption"], $conn, $images);
+    header("Location: create_post.php");
 }
