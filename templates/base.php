e2e636357631573ca8572e2a2aba9e311fba2955
expl0r3rgu1 <salvaddimando@gmail.com>
2022-12-31 10:41:04 +0100

    Added title attribute to anchor elements

--- templates/base.php
+++ templates/base.php
@@ -26,11 +26,11 @@
     </main>
     <footer>
         <div class="navbar">
-            <a href="index.php"><span class="fa-regular fa-house"></span></a>
-            <a href="search.php"><span class="fa-regular fa-magnifying-glass"></span></a>
-            <a href="create_post.php"><span class="fa-regular fa-plus"></span></a>
-            <a href="notifications.php"><span class="fa-regular fa-bell"></span></a>
-            <a href="profile.php"><span class="fa-regular fa-user"></span></a>
+            <a href="index.php" title="Home Page"><span class="fa-regular fa-house"></span></a>
+            <a href="search.php" title="Search Page"><span class="fa-regular fa-magnifying-glass"></span></a>
+            <a href="create_post.php" title="Create Post Page"><span class="fa-regular fa-plus"></span></a>
+            <a href="notifications.php" title="Notifications Page"><span class="fa-regular fa-bell"></span></a>
+            <a href="profile.php" title="Profile Page"><span class="fa-regular fa-user"></span></a>
         </div>
     </footer>
 </body>
