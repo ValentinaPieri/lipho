f99b1c1cea9fa37ba99aa0e5a5146610719e48f1
expl0r3rgu1 <salvaddimando@gmail.com>
2023-01-02 12:59:53 +0100

    Defined templateParams scripts and page in search.php

--- search.php
+++ search.php
@@ -1,4 +1,20 @@
 <?php
 $templateParams["title"] = "Search";
 
+$templateParams["scripts"] = "
+    <script src=\"https://code.jquery.com/jquery-3.5.0.js\"></script>
+    <script src=\"js/search.js\"></script>
+";
+
+$templateParams["page"] = "
+    <div class=\"search-container\">
+        <input type=\"text\" id=\"search-input\" placeholder=\"Search\">
+        <button type=\"button\" id=\"search-button\" onClick=\"getMatchingUsers(document.getElementById('search-input').value)\">
+            <span class=\"fa-regular fa-magnifying-glass\"></span>
+        </button>
+    </div>
+    <div id=\"search-results-container\">
+    </div>
+";
+
 require_once 'templates/base.php';
