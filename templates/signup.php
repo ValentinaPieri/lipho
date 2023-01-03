4af050b4353892c25acb6e4a3d60d98d12a62c5a
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2023-01-03 18:16:24 +0100

    added form and deleted required from username, password1, password2, name and surname

--- templates/signup.php
+++ templates/signup.php
@@ -17,18 +17,18 @@
                 <h1 id="SignUp">Sign up</h1>
             </div>
             <div class="container">
-
-                <i class="fa-solid fa-asterisk"></i><input type="text" name="username" id="username" placeholder="username" required><br>
-                <i class="fa-solid fa-asterisk"></i><input type="password" name="password1" id="password1" placeholder="password" required><br>
-                <i class="fa-solid fa-asterisk"></i><input type="password" name="password2" id="password2" placeholder="confirm password" required><br>
-                <i class="fa-solid fa-asterisk"></i><input type="text" name="name" id="name" placeholder="name" required><br>
-                <i class="fa-solid fa-asterisk"></i><input type="text" name="surname" id="surname" placeholder="surname" required><br>
+            <form>
+                <i class="fa-solid fa-asterisk"></i><input type="text" name="username" id="username" placeholder="username"><br>
+                <i class="fa-solid fa-asterisk"></i><input type="password" name="password1" id="password1" placeholder="password"><br>
+                <i class="fa-solid fa-asterisk"></i><input type="password" name="password2" id="password2" placeholder="confirm password"><br>
+                <i class="fa-solid fa-asterisk"></i><input type="text" name="name" id="name" placeholder="name"><br>
+                <i class="fa-solid fa-asterisk"></i><input type="text" name="surname" id="surname" placeholder="surname"><br>
                 <input type="text" name="email" id="email" placeholder="email"><br>
                 <input type="tel" name="phone" id="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="phone ex. xxx-xxx-xxxx"><br>
                 <label for="birthday">birthday:</label>&nbsp;
                 <input type="date" id="birthdate" name="birthdate"><i class="fa-regular fa-calendar"></i><br><br>
                 <button type="button" onclick="submitForm(document.getElementById('username').value, document.getElementById('password1').value, document.getElementById('password2').value, document.getElementById('name').value, document.getElementById('surname').value, document.getElementById('email').value, document.getElementById('phone').value, document.getElementById('birthdate').value)">Sign up</button>
-
+            </form>
             </div>
         </div>
         <div class="option">
