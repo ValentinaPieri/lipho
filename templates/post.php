4f6c14a63ccbe0ee5a7613cd1df32bbde4c24db6
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2023-01-07 13:07:18 +0100

    template refactoring

--- templates/post.php
+++ templates/post.php
@@ -1,51 +1,55 @@
-<script src='js/posts.js'></script>
-<script src='https://code.jquery.com/jquery-3.5.0.js'></script>
+<?php
+if (isset($templateParams["post"])) {
+    $POST_TEMPLATE = "
+        <div class=\"post\">
+            <div class=\"post-images\" id=\"post-images\">
+                <div class=slide-index id=\"slide-index\"></div>
+    ";
 
-<div class="post">
-    <div class="post-images" id="post-images">
-        <div class=slide-index id="slide-index"></div>
-        <?php
-        if (isset($templateParams["post"]["images"])) {
-            $postImagesNum = count($templateParams["post"]["images"]);
-            for ($i = 0; $i < $postImagesNum; $i++) {
-                echo '<script>';
-                echo 'i = "' . $i . '";';
-                echo '</script>';
-                $src = base64_encode($templateParams['post']['images'][$i]);
-                echo '<script>';
-                echo 'src = "' . $src . '";';
-                echo 'displayPostImages(src, i);';
-                echo '</script>';
-            }
-            if ($postImagesNum != 1) {
-                echo '<script>showSlide(1)</script>';
-            }
-            echo '<script>postId = "' . $templateParams["post"]["post_id"] . '";';
-            echo 'displayPostImagesButtons(postId, true)</script>';
-        }
-        ?>
-    </div>
-    <div class="post-comments" id="post-comments">
-        <?php
-        if (isset($templateParams["post"])) {
-            echo '<script>showPostCommentSection()</script>';
-            foreach ($templateParams["post"]["comments"] as $comment) {
-                echo "<p class='comment-username'>" . $comment->getUsername() . "</p>";
-                echo "<p class='comment-text'>" . $comment->getText() . "</p>";
-                echo "<button id='reply-button' type='button'>Reply</button>";
-            }
-        }
-        ?>
-    </div>
-    <div class="post-rating" id="post-rating">
-        <?php echo "<script>showPostRatingSection();</script>"; ?>
-    </div>
-    <div class="post-caption">
-        <?php
-        if (isset($templateParams["post"])) {
-            echo "<p class='caption-username'>" . $templateParams["post"]["username"] . "</p>";
-            echo "<p class='caption-text'>" . $templateParams["post"]["caption"] . "</p>";
-        }
-        ?>
-    </div>
-</div>
+    $postImagesNum = count($templateParams["post"]["images"]);
+    $POST_TEMPLATE .= "
+        <script>
+    ";
+    for ($i = 0; $i < $postImagesNum; $i++) {
+        $src = base64_encode($templateParams["post"]["images"][$i]);
+        $POST_TEMPLATE .= "
+            displayPostImages(\"" . $src . "\", " . $i . ");
+        ";
+    }
+    if ($postImagesNum != 1) {
+        $POST_TEMPLATE .= "
+            showSlide(1);
+        ";
+    }
+    $POST_TEMPLATE .= "
+        displayPostImagesButtons(" . $templateParams["post"]["post_id"] . ");</script>
+    ";
+
+    $POST_TEMPLATE .= " 
+        </div>
+        <div class=\"post-comments\" id=\"post-comments\">
+        </div>
+    ";
+
+    $POST_TEMPLATE .= "
+        <script>showPostCommentSection(" . $templateParams["post"]["post_id"] . ");</script>
+    ";
+    foreach ($templateParams["post"]["comments"] as $comment) {
+        $POST_TEMPLATE .= "
+            <p class=\"comment-username\">" . $comment->getUsername() . "</p>
+            <p class=\"comment-text\">" . $comment->getText() . "</p>
+            <button id=\"reply-button\" type=\"button\">Reply</button>
+        ";
+    }
+
+    $POST_TEMPLATE .= "
+            <div class=\"post-rating\" id=\"post-rating\">
+                <script>showPostRatingSection();</script>
+            </div>
+            <div class=\"post-caption\">
+                <p class=\"caption-username\">" . $templateParams["post"]["username"] . "</p>
+                <p class=\"caption-text\">" . $templateParams["post"]["caption"] . "</p>
+            </div>
+        </div>
+    ";
+}
