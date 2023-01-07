1bcc443927168efe8c0f49a1c05a387b3ed85a6c
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2023-01-07 17:57:22 +0100

    Created static html elements in php

--- js/posts.js
+++ js/posts.js
@@ -1,61 +1,38 @@
-let slideIndex = [1, 1];
+let currentSlide = 0;
 let postImageNum = 0;
-
-function displayPostImages(src, imageIndex) {
-    document.getElementById("post-images").innerHTML += "<img class='post-image-slide' id='post image" + imageIndex + "' src='data:image/jpeg;base64," + src + "' alt='Post image " + imageIndex + "'></img>";
-    postImageNum++;
-}
 
 function displayPostImageNumber(imageIndex, totalImagesNumber) {
     document.getElementById("slide-index").innerHTML = "<p>" + ++imageIndex + "/" + totalImagesNumber + "</p>";
 }
 
-function displayPostImagesButtons(postId) {
-    if (postImageNum > 1) {
-        document.getElementById("post-images").innerHTML += "<button class='post-button' id='previous-slide-button' type='button' onclick='changeSlide(-1)'><span class='fa-regular fa-arrow-left'></span></button>";
-        document.getElementById("post-images").innerHTML += "<button class='post-button' id='next-slide-button' type='button' onclick='changeSlide(1)'><span class='fa-regular fa-arrow-right'></span></button>";
-    }
-    document.getElementById("post-images").innerHTML += "<button class='post-button' id='full-screen-button' type='button'><span class='fa-regular fa-expand'></span></button>";
-    document.getElementById("post-images").innerHTML += "<button class='post-button' id='like-button' type='button' onclick='likePost(" + postId + ")'><span class='fa-regular fa-heart'></span></button>";
-    document.getElementById("post-images").innerHTML += "<button class='post-button' id='comment-button' type='button'><span class='fa-regular fa-comment-dots'></span></button>";
-    document.getElementById("post-images").innerHTML += "<button class='post-button' id='rating-button' type='button'><span class='fa-regular fa-square-star'></span></button>";
-}
-
-function showSlide(step) {
-    let i;
+function showSlideLeft() {
     let slide = document.getElementsByClassName('post-image-slide');
-    if (step > slide.length) { slideIndex[0] = 1 }
-    if (step < 1) { slideIndex[0] = slide.length }
-    for (i = 0; i < slide.length; i++) {
-        slide[i].hidden = true;
+    if (currentSlide > 0) {
+        slide[currentSlide].hidden = true;
+        currentSlide--;
+        slide[currentSlide].hidden = false;
+    } else {
+        slide[currentSlide].hidden = true;
+        currentSlide = slide.length - 1;
+        slide[currentSlide].hidden = false;
     }
-    displayPostImageNumber(slideIndex[0] - 1, slide.length);
-    slide[slideIndex[0] - 1].hidden = false;
 }
 
-function changeSlide(step) {
-    showSlide(slideIndex[0] += step);
+function showSlideRight() {
+    let slide = document.getElementsByClassName('post-image-slide');
+    if (currentSlide < slide.length - 1) {
+        slide[currentSlide].hidden = true;
+        currentSlide++;
+        slide[currentSlide].hidden = false;
+    } else {
+        slide[currentSlide].hidden = true;
+        currentSlide = 0;
+        slide[currentSlide].hidden = false;
+    }
 }
 
-function showPostCommentSection(postId) {
-    // console.log(postId)
-    document.getElementById("post-comments").innerHTML += "<input class='post-comment-input' id='post-comment-input' title='comment text input area' aria-label='comment text input area' type='text' placeholder='Type here your comment'/>";
-    document.getElementById("post-comments").innerHTML += "<button class='post-button' id='submit-comment-button' type='button' onclick='commentPost(" + postId + ",document.getElementById(\"post-comment-input\").value)'><span class='fa-regular fa-paper-plane-top'></span></button>";
-}
-
-function showPostRatingSection() {
-    document.getElementById("post-rating").innerHTML += "<form method='post' id='ratings-form'></form>";
-    document.getElementById("ratings-form").innerHTML += "<label for='exposure-rating'>Exposure</label>";
-    document.getElementById("ratings-form").innerHTML += "<input class='rating-input' id='exposure-rating' type='range' min='0' max='5' value='5'>";
-    document.getElementById("ratings-form").innerHTML += "<label for='color-rating'>Color</label>";
-    document.getElementById("ratings-form").innerHTML += "<input class='rating-input' id='color-rating' type='range' min='0' max='5' value='5'>";
-    document.getElementById("ratings-form").innerHTML += "<label for='composition-rating'>Color</label>";
-    document.getElementById("ratings-form").innerHTML += "<input class='rating-input' id='composition-rating' type='range' min='0' max='5' value='5'>";
-    document.getElementById("ratings-form").innerHTML += "<button class='rating-submit-button' id='rating-submit-button' type='submit'>Rate</button>";
-}
-
-function likePost(postId) {
-    $.post("/lipho/post_requests_handler.php", { postId: postId, likePost: true })
+function toggleLikePost(postId) {
+    $.post("/lipho/post_requests_handler.php", { postId: postId, toggleLikePost: true })
         .done(function () {
             document.getElementById("like-button").innerHTML = "<span class='fa-solid fa-heart'></span>";
         });
--- templates/post.php
+++ templates/post.php
@@ -1,38 +1,42 @@
 <?php
 if (isset($templateParams["post"])) {
+    $postImagesNum = count($templateParams["post"]["images"]);
     $POST_TEMPLATE = "
         <div class=\"post\">
             <div class=\"post-images\" id=\"post-images\">
-                <div class=\"slide-index\" id=\"slide-index\"></div>
     ";
-
-    $postImagesNum = count($templateParams["post"]["images"]);
-    $POST_TEMPLATE .= "
-        <script>
-    ";
-    for ($i = 0; $i < $postImagesNum; $i++) {
-        $src = base64_encode($templateParams["post"]["images"][$i]);
+    if ($postImagesNum > 1) {
         $POST_TEMPLATE .= "
-            displayPostImages(\"" . $src . "\", " . $i . ");
+                <div class=\"slide-index\" id=\"slide-index\"><p>1/" . $postImagesNum . "</p></div>
         ";
     }
-    if ($postImagesNum != 1) {
+
+    $POST_TEMPLATE .= "
+        <img class='post-image-slide' id='post-image0' src='data:image/jpeg;base64," . base64_encode($templateParams["post"]["images"][0]) . "' alt='Post image 0'></img>
+    ";
+    for ($i = 1; $i < $postImagesNum; $i++) {
         $POST_TEMPLATE .= "
-            showSlide(1);
+            <img class='post-image-slide' id='post-image" . $i . "' src='data:image/jpeg;base64," . base64_encode($templateParams["post"]["images"][$i]) . "' alt='Post image " . $i . "' hidden></img>
         ";
     }
-    $POST_TEMPLATE .= "
-        displayPostImagesButtons(" . $templateParams["post"]["post_id"] . ");</script>
-    ";
 
-    $POST_TEMPLATE .= " 
+    if ($postImagesNum > 1) {
+        $POST_TEMPLATE .= "
+            <button class='post-button' id='previous-slide-button' type='button' onclick='showSlideLeft()'><span class='fa-regular fa-arrow-left'></span></button>
+            <button class='post-button' id='next-slide-button' type='button' onclick='showSlideRight()'><span class='fa-regular fa-arrow-right'></span></button>
+        ";
+    }
+
+    $POST_TEMPLATE .= "
+            <button class='post-button' id='full-screen-button' type='button'><span class='fa-regular fa-expand'></span></button>
+            <button class='post-button' id='like-button' type='button' onclick='toggleLikePost(" . $templateParams["post"]["post_id"] . ")'><span class='fa-regular fa-heart'></span></button>
+            <button class='post-button' id='comment-button' type='button'><span class='fa-regular fa-comment-dots'></span></button>
+            <button class='post-button' id='rating-button' type='button'><span class='fa-regular fa-square-star'></span></button>
         </div>
         <div class=\"post-comments\" id=\"post-comments\">
+            <input class='post-comment-input' id='post-comment-input' title='comment text input area' aria-label='comment text input area' type='text' placeholder='Type here your comment'/>
+            <button class='post-button' id='submit-comment-button' type='button' onclick='commentPost(" . $templateParams["post"]["post_id"] . ", document.getElementById(\"post-comment-input\").value)'><span class='fa-regular fa-paper-plane-top'></span></button>
         </div>
-    ";
-
-    $POST_TEMPLATE .= "
-        <script>showPostCommentSection(" . $templateParams["post"]["post_id"] . ");</script>
     ";
     foreach ($templateParams["post"]["comments"] as $comment) {
         $POST_TEMPLATE .= "
@@ -44,7 +48,13 @@ if (isset($templateParams["post"])) {
 
     $POST_TEMPLATE .= "
             <div class=\"post-rating\" id=\"post-rating\">
-                <script>showPostRatingSection();</script>
+                <label for='exposure-rating'>Exposure</label>
+                <input class='rating-input' id='exposure-rating' type='range' min='0' max='5' value='5'>
+                <label for='color-rating'>Color</label>
+                <input class='rating-input' id='color-rating' type='range' min='0' max='5' value='5'>
+                <label for='composition-rating'>Color</label>
+                <input class='rating-input' id='composition-rating' type='range' min='0' max='5' value='5'>
+                <button class='rating-submit-button' id='rating-submit-button' type='button'>Rate</button>
             </div>
             <div class=\"post-caption\">
                 <p class=\"caption-username\">" . $templateParams["post"]["username"] . "</p>
