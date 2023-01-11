2d7160a2f50ccb2095f90c2f9fa5ef900bcc768d
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2023-01-11 17:05:25 +0100

    changed use of insert function, changed its name into showInsertError and changed name of notValid into showInvalidError

--- js/signup.js
+++ js/signup.js
@@ -1,20 +1,35 @@
 function submitForm(username, password1, password2, name, surname, email, phone, birthdate) {
+    const inputs = [...document.getElementsByTagName("input")];
+    inputs.forEach(input => {
+        input.style.borderColor = "black";
+    });
     $.post("/lipho/post_requests_handler.php", { signup: true, username: username, password1: password1, password2: password2, name: name, surname: surname, email: email, phone: phone, birthdate: birthdate }, function (result) {
-        if(username==="" || password1==="" || password2==="" || name==="" || surname==="" ){
-        //TODO: to complete
-            insert
+        if (username === "") {
+            showInsertError("username");
+        } else if (result.usernameValid === false) {
+            showInvalidError("username");
         }
-        if (result.usernameValid === false) {
-            notValid("username");
+        if (password1 === "") {
+            showInsertError("password1");
+        } else if (result.passwordValid === false) {
+            showInvalidError("password1");
         }
-        if (result.passwordsMatching === false) {
-            notValid("password2");
+        if (password2 === "") {
+            showInsertError("password2");
+        } else if (result.passwordsMatching === false) {
+            showInvalidError("password2");
+        }
+        if (name === "") {
+            showInsertError("name");
+        }
+        if (surname === "") {
+            showInsertError("surname");
         }
         if (result.phoneValid === false) {
-            notValid("phone");
-        } 
+            showInvalidError("phone");
+        }
         if (result.emailValid === false) {
-            notValid("email");
+            showInvalidError("email");
         }
         //TODO: to complete after doing login.php
         // else{
@@ -23,19 +38,19 @@ function submitForm(username, password1, password2, name, surname, email, phone,
     }, "json");
 }
 
-function insert($value){
+function showInsertError($value) {
     document.getElementById($value).style.borderColor = "red";
-    document.getElementById($value).placeholder = "Please insert "+$value;
+    document.getElementById($value).placeholder = "Please insert " + $value;
 }
 
-function notValid($value){
+function showInvalidError($value) {
     document.getElementById($value).style.borderColor = "red";
     document.getElementById($value).value = "";
-    if($value!=="password2" && $value!=="username"){
-        document.getElementById($value).placeholder = "Insert a valid "+$value;
-    }else if($value==="username"){
+    if ($value !== "password2" && $value !== "username") {
+        document.getElementById($value).placeholder = "Insert a valid " + $value;
+    } else if ($value === "username") {
         document.getElementById($value).placeholder = "Username already exists";
-    }else{
+    } else {
         document.getElementById($value).placeholder = "Passwords don't match";
     }
 }
