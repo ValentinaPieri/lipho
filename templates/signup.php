7733626e574b89f7c56df5cf1785de0c0b19c072
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2023-01-11 18:43:26 +0100

    made signup.php accessible

--- templates/signup.php
+++ templates/signup.php
@@ -18,15 +18,15 @@
             </div>
             <div class="container">
             <form>
-                <i class="fa-solid fa-asterisk"></i><input type="text" name="username" id="username" placeholder="username">
-                <i class="fa-solid fa-asterisk"></i><input type="password" name="password1" id="password1" placeholder="password">
-                <i class="fa-solid fa-asterisk"></i><input type="password" name="password2" id="password2" placeholder="confirm password">
-                <i class="fa-solid fa-asterisk"></i><input type="text" name="name" id="name" placeholder="name">
-                <i class="fa-solid fa-asterisk"></i><input type="text" name="surname" id="surname" placeholder="surname">
-                <input type="text" name="email" id="email" placeholder="email">
-                <input type="tel" name="phone" id="phone" placeholder="ex. +393213334444">
-                <label for="birthday">birthday:</label>&nbsp;
-                <input type="date" id="birthdate" name="birthdate"><i class="fa-regular fa-calendar"></i>
+                <span class="fa-solid fa-asterisk"><span><input title="username" aria-label="username input form" type="text" name="username" id="username" placeholder="username">
+                <span class="fa-solid fa-asterisk"><span><input title="password" aria-label="password input form" type="password" name="password1" id="password1" placeholder="password">
+                <span class="fa-solid fa-asterisk"><span><input title="confirm password" aria-label="confirm password input form" type="password" name="password2" id="password2" placeholder="confirm password">
+                <span class="fa-solid fa-asterisk"><span><input title="name" aria-label="name input form" type="text" name="name" id="name" placeholder="name">
+                <span class="fa-solid fa-asterisk"><span><input title="surname" aria-label="surname input form" type="text" name="surname" id="surname" placeholder="surname">
+                <input title="email" aria-label="email input form" type="text" name="email" id="email" placeholder="email">
+                <input title="phone" aria-label="phone input form" type="tel" name="phone" id="phone" placeholder="ex. +393213334444">
+                <label for="birthdate">birthday:</label>&nbsp;
+                <input title="birthdate" type="date" id="birthdate" name="birthdate"><span class="fa-regular fa-calendar"><span>
                 <button type="button" onclick="submitForm(document.getElementById('username').value, document.getElementById('password1').value, document.getElementById('password2').value, document.getElementById('name').value, document.getElementById('surname').value, document.getElementById('email').value, document.getElementById('phone').value, document.getElementById('birthdate').value)">Sign up</button>
             </form>
             </div>
