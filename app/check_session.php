2fac31d62d82f2051c5003a92cd20d74169ab200
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2023-01-12 23:13:12 +0100

    changed signin.php into login.php

--- app/check_session.php
+++ app/check_session.php
@@ -4,10 +4,10 @@ session_start();
 
 $page = basename($_SERVER['PHP_SELF']);
 
-if (isset($_SESSION["loggedin"]) && $page == "signin.php" || $page == "signup.php") {
+if (isset($_SESSION["loggedin"]) && $page == "login.php" || $page == "signup.php") {
     header("Location: index.php");
     exit;
-} else if (!isset($_SESSION["loggedin"]) && $page != "signin.php" && $page != "signup.php") {
-    header("Location: signin.php");
+} else if (!isset($_SESSION["loggedin"]) && $page != "login.php" && $page != "signup.php") {
+    header("Location: login.php");
     exit;
 }
