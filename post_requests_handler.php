96db3d6a5aa143db0c4212afb4e184f60ab741b9
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2023-01-12 13:14:19 +0100

    changed login request and add use of etUserLoggedIn in post_request_handler.php

--- post_requests_handler.php
+++ post_requests_handler.php
@@ -69,11 +69,14 @@ if (isset($_POST['getMatchingUsers'])) {
     echo json_encode($users);
 }
 
-if (isset($_POST['login'])){
+if (isset($_POST['login'])) {
     $dbconnection = new DBConnection();
-    $result["usernameValid"] = $dbconnection->checkUsername($_POST['username']);
-    if ($result["usernameValid"]){
+    $result["usernameValid"] = !$dbconnection->checkUsername($_POST['username']);
+    if ($result["usernameValid"]) {
         $result["passwordValid"] = $dbconnection->checkPassword($_POST['username'], $_POST['password']);
+        if($result["passwordValid"]) {
+            $dbconnection->setUserLoggedIn($_POST['username']);
+        }
     }
     echo json_encode($result);
 }
