ec790cf7eab1a2002a9e31f088022717e06ea56b
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2023-01-12 19:34:33 +0100

    added templateParams["scripts"] for feed.js and post.js

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
 $templateParams["title"] = "Home";
-
+$templateParams["scripts"] = "
+    <script src='js/feed.js'></script>
+    <script src='js/post.js'></script>
+";
 require_once 'templates/base.php';
