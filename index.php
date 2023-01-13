7d57031dd9ac28e8021ed63c3d1316908b15bc2f
Benedetta Pacilli <benedetta.pacilli@studio.unibo.it>
2023-01-13 12:25:09 +0100

    Removed changes in index.php

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
 <?php
-require_once 'app/models/Post.php';
-require_once 'app/DBConnection.php';
-
-use App\Models\Post;
-use App\DBConnection;
-
 $templateParams["title"] = "Home";
-
-$templateParams["scripts"] = "
-    <script src=\"https://code.jquery.com/jquery-3.5.0.js\"></script>
-    <script src=\"js/feed.js\"></script>
-    <script src=\"js/post.js\"></script>
-";
-
-$templateParams["page"] = "";
 
 require_once 'templates/base.php';
