09980ad4bd1a47f1b73b9e9c92ee71f59d0d1965
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2023-01-13 12:35:51 +0100

    Moved templateParams["page"] content from search.php to search_page.php

--- search.php
+++ search.php
@@ -5,12 +5,6 @@ $templateParams["scripts"] = "
     <script src=\"js/search.js\"></script>
 ";
 
-$templateParams["page"] = "
-    <div class=\"search-container\">
-        <input type=\"text\" id=\"search-input\" placeholder=\"Search\">
-    </div>
-    <div id=\"search-results-container\">
-    </div>
-";
+$templateParams["page"] = "search_page.php";
 
 require_once 'templates/base.php';
