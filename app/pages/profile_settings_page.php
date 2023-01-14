aa2f1d6c96aea3841f2cf9c396ac31f76cf1c903
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2023-01-14 02:09:22 +0100

    Set name attribute for input elements to retrieve data in js

--- app/pages/profile_settings_page.php
+++ app/pages/profile_settings_page.php
@@ -3,13 +3,13 @@
         <button id="save-button" type="submit">Save</button>
         <label for="change-profile-image">Edit profile image</label><img id="change-profile-image" src="/lipho/resources/images/blank_profile_picture.jpeg" alt="Current profile image" />
         <p class="image-size-disclaimer">max image size:<br />15 MB</p>
-        <label for="edit-username">Edit username</label><input id="edit-username" type="text" />
-        <label for="edit-password">Edit password</label><input id="edit-password" type="password" />
-        <label for="edit-name">Edit name</label><input id="edit-name" type="text" />
-        <label for="edit-surname">Edit surname</label><input id="edit-surname" type="text" />
-        <label for="edit-email">Edit email</label><input id="edit-email" type="email" />
-        <label for="edit-phone-number">Edit phone number</label><input id="edit-phone-number" type="tel" />
-        <label for="edit-birthdate">Edit birthdate</label><input id="edit-birthdate" type="date" />
+        <label for="edit-username">Edit username</label><input id="edit-username" name="username" type="text" />
+        <label for="edit-password">Edit password</label><input id="edit-password" name="password" type="password" />
+        <label for="edit-name">Edit name</label><input id="edit-name" name="name" type="text" />
+        <label for="edit-surname">Edit surname</label><input id="edit-surname" name="surname" type="text" />
+        <label for="edit-email">Edit email</label><input id="edit-email" name="email" type="email" />
+        <label for="edit-phone-number">Edit phone number</label><input id="edit-phone-number" name="phone" type="tel" />
+        <label for="edit-birthdate">Edit birthdate</label><input id="edit-birthdate" name="birthdate" type="date" />
         <button type="button" id="logout-button">Log out</button><span class="fa-regular fa-arrow-right-from-bracket"></span>
         <button type="button" id="delete-account-button">Delete account</button><span class="fa-regular fa-trash-xmark"></span>
     </form>
