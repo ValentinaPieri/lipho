5485bc55b2b76f542d7b650582940960a446a621
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2023-01-15 15:50:35 +0100

    Detached images rendering from post container creation

--- js/post.js
+++ js/post.js
@@ -1,6 +1,6 @@
 let postsCurrentSlide = [];
 
-function getPostContainer(postId, owner, caption, images, comments, liked, rated, currentUsername) {
+function getPostContainer(postId, owner, caption, liked, rated) {
     postsCurrentSlide[postId] = 0;
     let postDiv = document.createElement("div");
     postDiv.className = "post";
@@ -9,56 +9,6 @@ function getPostContainer(postId, owner, caption, images, comments, liked, rated
     let postImagesDiv = document.createElement("div");
     postImagesDiv.className = "post-images";
     postImagesDiv.id = "post-images" + postId;
-
-    if (images.length > 1) {
-        let indexDiv = document.createElement("div");
-        indexDiv.className = "slide-index";
-        indexDiv.id = "slide-index" + postId;
-        let indexParagraph = document.createElement("p");
-        indexParagraph.id = "index" + postId;
-        indexParagraph.textContent = "1/" + images.length;
-        indexDiv.appendChild(indexParagraph);
-        postImagesDiv.appendChild(indexDiv);
-    }
-
-    for (let i = 0; i < images.length; i++) {
-        let postImageSlide = document.createElement("img");
-        postImageSlide.className = "post-image-slide";
-        postImageSlide.id = "post-image-slide" + postId + i;
-        postImageSlide.src = "data:image/jpeg;base64," + images[i];
-        postImageSlide.alt = "Post image " + i;
-        postImageSlide.hidden = i != 0;
-        postImagesDiv.appendChild(postImageSlide);
-    }
-
-    if (images.length > 1) {
-        let arrowButtonsDiv = document.createElement("div");
-        arrowButtonsDiv.className = "arrow-buttons-div";
-        let leftArrowButton = document.createElement("button");
-        leftArrowButton.className = "post-button arrow-button";
-        leftArrowButton.id = "previous-slide-button" + postId;
-        leftArrowButton.type = "button";
-        let leftArrowButtonIcon = document.createElement("span");
-        leftArrowButtonIcon.className = "fa-regular fa-arrow-left";
-        leftArrowButton.appendChild(leftArrowButtonIcon);
-        leftArrowButton.onclick = function () {
-            showSlideLeft(postId);
-        };
-        let rightArrowButton = document.createElement("button");
-        rightArrowButton.className = "post-button arrow-button";
-        rightArrowButton.id = "next-slide-button" + postId;
-        rightArrowButton.type = "button";
-        let rightArrowButtonIcon = document.createElement("span");
-        rightArrowButtonIcon.className = "fa-regular fa-arrow-right";
-        rightArrowButton.appendChild(rightArrowButtonIcon);
-
-        rightArrowButton.onclick = function () {
-            showSlideRight(postId);
-        };
-        arrowButtonsDiv.appendChild(leftArrowButton);
-        arrowButtonsDiv.appendChild(rightArrowButton);
-        postImagesDiv.appendChild(arrowButtonsDiv);
-    }
 
     let fullScreenButton = document.createElement("button");
     fullScreenButton.className = "post-button full-screen-button";
