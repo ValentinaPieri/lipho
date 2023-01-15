dd44810c4945e4b5c9f0a1ea517db217c4d10740
Salvatore Antonio Addimando <salvaddimando@gmail.com>
2023-01-15 15:50:52 +0100

    Created method retrieveImages in feed.js

--- js/feed.js
+++ js/feed.js
@@ -7,14 +7,72 @@ function getFeedPosts(offset, limit) {
         let postDiv;
         const mainTag = document.querySelector("main");
         result.posts.forEach(post => {
-            postDiv = getPostContainer(post.post_id, post.owner, post.caption, post.images, post.comments, post.liked, post.rated, result.currentUsername);
+            postDiv = getPostContainer(post.post_id, post.owner, post.caption, post.liked, post.rated);
             mainTag.appendChild(postDiv);
+            retrieveImages(post.post_id);
             retrieveLikesNumber(post.post_id);
             retrieveComments(post.post_id, result.currentUsername);
             setInterval(function () {
                 retrieveLikesNumber(post.post_id);
                 retrieveComments(post.post_id, result.currentUsername);
             }, 5000);
+        });
+    }, "json");
+}
+
+function retrieveImages(postId) {
+    $.post("./post_requests_handler.php", { getPostImages: true, post_id: postId }, function (images) {
+        let postImagesDiv = document.getElementById("post-images" + postId);
+        images.forEach(image => {
+            if (images.length > 1) {
+                let indexDiv = document.createElement("div");
+                indexDiv.className = "slide-index";
+                indexDiv.id = "slide-index" + postId;
+                let indexParagraph = document.createElement("p");
+                indexParagraph.id = "index" + postId;
+                indexParagraph.textContent = "1/" + images.length;
+                indexDiv.appendChild(indexParagraph);
+                postImagesDiv.appendChild(indexDiv);
+            }
+
+            for (let i = 0; i < images.length; i++) {
+                let postImageSlide = document.createElement("img");
+                postImageSlide.className = "post-image-slide";
+                postImageSlide.id = "post-image-slide" + postId + i;
+                postImageSlide.src = "data:image/jpeg;base64," + images[i];
+                postImageSlide.alt = "Post image " + i;
+                postImageSlide.hidden = i != 0;
+                postImagesDiv.appendChild(postImageSlide);
+            }
+
+            if (images.length > 1) {
+                let arrowButtonsDiv = document.createElement("div");
+                arrowButtonsDiv.className = "arrow-buttons-div";
+                let leftArrowButton = document.createElement("button");
+                leftArrowButton.className = "post-button arrow-button";
+                leftArrowButton.id = "previous-slide-button" + postId;
+                leftArrowButton.type = "button";
+                let leftArrowButtonIcon = document.createElement("span");
+                leftArrowButtonIcon.className = "fa-regular fa-arrow-left";
+                leftArrowButton.appendChild(leftArrowButtonIcon);
+                leftArrowButton.onclick = function () {
+                    showSlideLeft(postId);
+                };
+                let rightArrowButton = document.createElement("button");
+                rightArrowButton.className = "post-button arrow-button";
+                rightArrowButton.id = "next-slide-button" + postId;
+                rightArrowButton.type = "button";
+                let rightArrowButtonIcon = document.createElement("span");
+                rightArrowButtonIcon.className = "fa-regular fa-arrow-right";
+                rightArrowButton.appendChild(rightArrowButtonIcon);
+
+                rightArrowButton.onclick = function () {
+                    showSlideRight(postId);
+                };
+                arrowButtonsDiv.appendChild(leftArrowButton);
+                arrowButtonsDiv.appendChild(rightArrowButton);
+                postImagesDiv.appendChild(arrowButtonsDiv);
+            }
         });
     }, "json");
 }
