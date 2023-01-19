66a3b100b58f28c6231ecc58ccdf3d69d84ba9c8
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2023-01-19 17:45:44 +0100

    Added class name to input type search element

--- app/pages/search_page.php
+++ app/pages/search_page.php
@@ -1,4 +1,4 @@
 <div class="search-container">
-    <input type="search" id="search-input" placeholder="Start typing a username" />
+    <input type="search" class="search-bar" id="search-input" placeholder="Start typing a username" />
 </div>
 <div id="search-results-container"></div>
