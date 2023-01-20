5159a8fdd3a9d4c71d9f0470f424e90071a433d9
expl0r3rgu1 <salvaddimando@gmail.com>
2023-01-20 11:57:59 +0100

    Refactored create_post page

--- app/pages/create_post_page.php
+++ app/pages/create_post_page.php
@@ -1,18 +1,17 @@
-<form class="create-post-form" action='post_requests_handler.php' method='post' enctype='multipart/form-data'>
-    <div class="images-form">
-        <h2>Pictures</h2>
-        <p id='images-counter'></p>
-        <button type='button' class="icon-button" id='add-image-button'>
-            <span class='fa-regular fa-circle-plus'></span>
-        </button>
-    </div>
-    <div id='images-container' class="images-container">
-    </div>
+<div class="images-form">
+    <h2>Pictures</h2>
+    <p id='images-counter'></p>
+    <button type='button' class="icon-button" id='add-image-button'>
+        <span class='fa-regular fa-circle-plus'></span>
+    </button>
+</div>
+<div id='images-container' class="images-container">
+</div>
 
-    <div id='caption-form'>
-        <h2><label for='caption'>Caption</label></h2>
-        <textarea id='caption' name='caption'></textarea>
-    </div>
+<div id='caption-form'>
+    <h2><label for='caption'>Caption</label></h2>
+    <textarea id='caption' name='caption' maxlength="200"></textarea>
+    <span id="remaining-characters">0/200</span>
+</div>
 
-    <button type='submit' class='text-button' name='createPost'>Post</button>
-</form>
+<button type='button' class='text-button' id='create-post-button'>Post</button>
--- js/create_post.js
+++ js/create_post.js
@@ -1,22 +1,33 @@
 const addImageButton = document.getElementById("add-image-button");
 const imagesContainer = document.getElementById("images-container");
+const captionTextArea = document.getElementById("caption");
+const maxLength = captionTextArea.maxLength;
+const remainingCharactersSpan = document.getElementById("remaining-characters");
+const createPostButton = document.getElementById("create-post-button");
 
 let images = [];
 const MAX_IMAGES = 5;
 
 addImageButton.onclick = addImage;
 
+captionTextArea.oninput = function () {
+    remainingCharactersSpan.innerText = captionTextArea.value.length + "/" + maxLength;
+}
+
+createPostButton.onclick = createPost;
+
 function addImage() {
     let fileInput = document.createElement("input");
     fileInput.type = "file";
     fileInput.id = "file-input";
-    fileInput.accept = "image/*";
+    fileInput.accept = ".jpg, .jpeg, .png";
     fileInput.onchange = function () {
         let file = fileInput.files[0];
         let reader = new FileReader();
         reader.onload = function () {
-            images.push(reader.result);
+            images.push(reader.result.substring(reader.result.indexOf(',') + 1));
             showImage(images.length - 1);
+            refreshArrowButtons();
             if (images.length >= MAX_IMAGES) {
                 addImageButton.hidden = true;
             }
@@ -36,6 +47,7 @@ function removeImage(imageIndex) {
     }
 
     refreshIndexes();
+    refreshArrowButtons();
 }
 
 function showImage(index) {
@@ -46,7 +58,7 @@ function showImage(index) {
     let imageElement = document.createElement('img');
     imageElement.id = 'image-element' + index;
     imageElement.alt = 'Image ' + index;
-    imageElement.src = images[index];
+    imageElement.src = "data:images/jpeg;base64," + images[index];
 
     let postButtonsContainer = document.createElement('div');
     postButtonsContainer.className = 'post-buttons-div';
@@ -62,14 +74,6 @@ function showImage(index) {
         removeImage(index);
     };
 
-    if (index == MAX_IMAGES - 1) {
-        let moveUpButton = document.createElement("button");
-        moveUpButton.className = "icon-button post-button";
-        moveUpButton.id = "move-up-button" + index;
-        moveUpButton.type = "button";
-        //TODO: add arrowbutons
-    }
-
     removeButton.appendChild(removeButtonIcon);
     postButtonsContainer.appendChild(removeButton);
     imageContainer.appendChild(postButtonsContainer);
@@ -83,7 +87,7 @@ function refreshIndexes() {
         imageContainers[i].id = 'post-image-container' + i;
         let imageElement = imageContainers[i].querySelector('img');
         imageElement.id = 'image-element' + i;
-        imageElement.src = images[i];
+        imageElement.src = "data:images/jpeg;base64," + images[i];
         imageElement.alt = 'Image ' + i;
         let removeButton = imageContainers[i].querySelector("[id^='remove-button']");
         removeButton.id = 'remove-button' + i;
@@ -91,4 +95,92 @@ function refreshIndexes() {
             removeImage(i);
         };
     }
+}
+
+function refreshArrowButtons() {
+    let moveButtons = document.querySelectorAll("[id^='move-']");
+    moveButtons.forEach(function (button) {
+        button.remove();
+    });
+
+    let postButtonsContainers = document.getElementsByClassName('post-buttons-div');
+    if (postButtonsContainers.length > 1) {
+        for (let i = 0; i < postButtonsContainers.length; i++) {
+            if (i == 0) {
+                postButtonsContainers[i].appendChild(getMoveDownButton(i));
+            } else if (i == postButtonsContainers.length - 1) {
+                postButtonsContainers[i].appendChild(getMoveUpButton(i));
+            } else {
+                postButtonsContainers[i].appendChild(getMoveUpButton(i));
+                postButtonsContainers[i].appendChild(getMoveDownButton(i));
+            }
+        }
+    }
+}
+
+function getMoveUpButton(imageIndex) {
+    let moveUpButton = document.createElement("button");
+    moveUpButton.className = "icon-button post-button";
+    moveUpButton.id = "move-up-button" + imageIndex;
+    moveUpButton.type = "button";
+
+    let moveUpButtonIcon = document.createElement("span");
+    moveUpButtonIcon.className = "fa-solid fa-arrow-up";
+    moveUpButton.onclick = function () {
+        moveImageUp(imageIndex);
+    }
+
+    moveUpButton.appendChild(moveUpButtonIcon);
+    return moveUpButton;
+}
+
+
+function moveImageUp(imageIndex) {
+    let imageContainer = document.getElementById('post-image-container' + imageIndex);
+    let previousImageContainer = imageContainer.previousElementSibling;
+    imagesContainer.insertBefore(imageContainer, previousImageContainer);
+    let temp = images[imageIndex];
+    images[imageIndex] = images[imageIndex - 1];
+    images[imageIndex - 1] = temp;
+    refreshIndexes();
+    refreshArrowButtons();
+}
+
+function getMoveDownButton(imageIndex) {
+    let moveDownButton = document.createElement("button");
+    moveDownButton.className = "icon-button post-button";
+    moveDownButton.id = "move-down-button" + imageIndex;
+    moveDownButton.type = "button";
+
+    let moveDownButtonIcon = document.createElement("span");
+    moveDownButtonIcon.className = "fa-solid fa-arrow-down";
+    moveDownButton.onclick = function () {
+        moveImageDown(imageIndex);
+    }
+
+    moveDownButton.appendChild(moveDownButtonIcon);
+    return moveDownButton;
+}
+
+function moveImageDown(imageIndex) {
+    let imageContainer = document.getElementById('post-image-container' + imageIndex);
+    let nextImageContainer = imageContainer.nextElementSibling;
+    imagesContainer.insertBefore(nextImageContainer, imageContainer);
+    let temp = images[imageIndex];
+    images[imageIndex] = images[imageIndex + 1];
+    images[imageIndex + 1] = temp;
+    refreshIndexes();
+    refreshArrowButtons();
+}
+
+function createPost() {
+    let args = {
+        createPost: true,
+        images: images,
+        caption: document.getElementById('caption').value,
+    }
+
+    $.post('./post_requests_handler.php', args, function () {
+        location.reload();
+    });
 }
