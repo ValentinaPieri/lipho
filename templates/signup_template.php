df3123da4c36af423b3b51a3845f9ea9975fd225
expl0r3rgu1 <salvaddimando@gmail.com>
2023-01-25 14:58:58 +0100

    Changed email input type from text to email

--- templates/signup_template.php
+++ templates/signup_template.php
@@ -36,7 +36,7 @@
                     <div class="input-field">
                         <span class="fa-solid fa-asterisk"></span><input title="surname" aria-label="surname input form" type="text" name="surname" id="surname" placeholder="surname" />
                     </div>
-                    <input title="email" aria-label="email input form" type="text" name="email" id="email" placeholder="email" />
+                    <input title="email" aria-label="email input form" type="email" name="email" id="email" placeholder="email" />
                     <input title="phone" aria-label="phone input form" type="tel" name="phone" id="phone" placeholder="ex. +393213334444" />
                     <div class="birthday-field">
                         <label for="birthdate">birthday:</label>
